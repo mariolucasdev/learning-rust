b02ff12d68ba814d00910f5ec372842d42d25c85
Mário Lucas <mariolucasdev@gmail.com>
2023-11-15 17:31:00 -0300

    variables and mutability

---
+++ variables-mutability/target/debug/deps/variables_mutability-e79091031f303572.d
@@ -0,0 +1,5 @@
+/home/mariolucas/rust/variables-mutability/target/debug/deps/variables_mutability-e79091031f303572: src/main.rs
+
+/home/mariolucas/rust/variables-mutability/target/debug/deps/variables_mutability-e79091031f303572.d: src/main.rs
+
+src/main.rs:
---
+++ variables-mutability/target/debug/variables-mutability.d
@@ -0,0 +1,1 @@
+/home/mariolucas/rust/variables-mutability/target/debug/variables-mutability: /home/mariolucas/rust/variables-mutability/src/main.rs
